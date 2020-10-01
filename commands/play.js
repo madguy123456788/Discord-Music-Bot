c1fe14fd7c295fc013b919c230c578a31158f8ad
madguy123456788 <1234madguy@gmail.com>
2020-10-01 15:28:04 +0100

    Embed playqueue handover in play.js

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,5 @@
 const ytdl = require('ytdl-core');
-//const { prefix } = require('../config.json');
-const prefix = process.env.PREFIX || "!";
-const { nextSong } = require('../modules/music');
+//const prefix = process.env.PREFIX || "!";
 module.exports = {
     name: "play",
     description: "Play a Song",
@@ -59,7 +57,7 @@ module.exports = {
                 //message.channel.send(`**Now Playing ${title || playqueue[0]}**`);
 
                 dispatcher.on('end', () => {
-                    nextSong(message);
+                    module.exports.nextSong(message);
                 });
             });
         } else {
@@ -71,5 +69,45 @@ module.exports = {
         }
 
         process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+    },
+    nextSong: async function(message) {
+        let guild = message.guild;
+        if (!guild.voiceData.stopping) {
+            guild.voiceData.queue.shift();
+            guild.voiceData.requesters.shift();
+            if (guild.voiceData.queue.length < 1) {
+                guild.voiceData.channel.leave();
+                guild.voiceData = undefined;
+            } else {
+                var info = await ytdl.getBasicInfo(guild.voiceData.queue[0]);
+                var title = info.videoDetails.title;
+                var time = `${Math.floor(info.videoDetails.lengthSeconds / 60)}:${info.videoDetails.lengthSeconds - (Math.floor(info.videoDetails.lengthSeconds / 60) * 60)}`
+                let stream = ytdl(guild.voiceData.queue[0], { filter: 'audioonly', highWaterMark: 1<<25 });
+                let dispatcher = guild.voiceData.connection.playStream(stream);
+                guild.voiceData.dispatcher = dispatcher;
+                var requester = guild.voiceData.requesters[0].nickname || guild.voiceData.requesters[0].user.username
+                //message.channel.send(`**Now Playing: ${guild.voiceData.queue[0]}**`)
+                const embed = {
+                	title: "Now Playing",
+                	color: 0xFF0000,
+                	fields: [
+                			{ name: "Title", value: title || "Error Getting Title" },
+                            { name: "URL", value: guild.voiceData.queue[0] },
+                            { name: "Length", value: time },
+                            { name: "Requester", value: requester }
+                		],
+                	timestamp: new Date(),
+                	footer: { text: `${message.client.user.username} • Brought to you By ${message.member.nickname || message.member.user.username}` }
+                };
+                
+                message.channel.send({ embed: embed });
+                console.log(`[${new Date()}] [${guild.name}] Now Playing Changed to "${title}"[${guild.voiceData.queue[0]}]`);
+                //message.channel.send(`**Now Playing: ${title || guild.voiceData.queue[0]}**`);
+
+                dispatcher.on('end', () => {
+                    module.exports.nextSong(message);
+                });
+            }
+        }
     }
 };
